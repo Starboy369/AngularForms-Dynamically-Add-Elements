6a8999d7f7ee63f2eb6a94456b86f47e986efa29
Starboy369 <rgakshay336@gmail.com>
2020-04-11 19:01:55 +0530

    4 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,34 @@
 import { Component } from '@angular/core';
-
+import { FormControl, FormGroup, FormArray, FormBuilder, Validators } from '@angular/forms';
 @Component({
   selector: 'my-app',
   templateUrl: './app.component.html',
   styleUrls: [ './app.component.css' ]
 })
-export class AppComponent  {
+export class AppComponent implements OnInit {
   name = 'Angular';
+ form_new: FormGroup
+  constructor(private fb: FormBuilder){}
+ ngOnInit() {
+   this.form_new = this.fb.group({
+     classroom:[''],
+     childrens: new FormArray([])
+   })
+   this.t.push(this.fb.group({
+     name:['']
+   }))
+
+ }
+  get f() { return this.form_new.controls; }
+ get t() { return this.f.childrens as FormArray; }
+ 
+ add(){
+    this.t.push(this.fb.group({
+     name:['']
+   }))
+ }
+ delete(val){
+    this.t.removeAt(val)
+ }
+
 }
